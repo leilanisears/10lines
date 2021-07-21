536ef0ad67ce751d7d69641d0aac8fd259ae9523
leilanisears <lsears1@asu.edu>
2021-07-21 02:27:27 -0700

    Added alert for messages

--- honeypot.py
+++ honeypot.py
@@ -10,16 +10,39 @@ def send_alert(ip_address):
     receiver_email = "leilani.sears@gmail.com"
 
     port = 465
-    password = input("Input password > ")
+
+    #hardcode password and pray
+    #password = ""
 
     context = ssl.create_default_context()
-    message = """\
+    messge = """\
         Subject: Connection Alert
         Honeypot has been connected to by %s on port %s""" % (ip_address, port)
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
+
+def msg_received(msg, ip_address):
+    sender_mail = "leilani.sears@gmail.com"
+    receiver_mail = "leilani.sears@gmail.com"
+
+    port = 465
+
+    #hardcode password
+    #password = ""
+
+    context = ssl.create_default_context()
+    message = """\
+        Sender %s has sent the following message:
+
+        %s
+
+        Message stored in logs.""" % (ip_address, msg)
+
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.send_mail(sender_email, receiver_email, message)
 
 # JULY 17TH
 # ------------------------------------------------------------------
